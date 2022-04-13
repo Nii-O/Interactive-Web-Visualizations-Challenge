41439f1ff12c10648202998b09cf5f29ce9da8e2
Nii <inodarteifio@gmail.com>
2022-04-13 11:04:17 -0400

    upload

--- Instructions/StarterCode/static/js/app.js
+++ Instructions/StarterCode/static/js/app.js
@@ -3,96 +3,107 @@ function init() {
         console.log("read samples");
         // console.log(data);
         // console.log(data.samples[1]['sample_values'])
-
-        sample_vals=[];
-        otu_idss=[];
-        otu_labelss=[];
-        metadata_ids=[];
-
-
+        buildCharts(data)
         var dropdown = d3.select('#selDataset');
-        data.names.forEach((name)=>{
+            data.names.forEach((name)=>{
             dropdown.append('option').text(name).property('value',name);
         })
-
         
 
 
-        for (let i = 0; i<data.samples.length; i++){
-            sample_vals.push( data.samples[i]['sample_values'])
-            otu_idss.push(data.samples[i]['otu_ids'])
-            otu_labelss.push(data.samples[i]['otu_labels'])
-            metadata_ids.push(data.metadata[i]['id'])
 
-        }    
-        console.log(metadata_ids);
-
-        
-
-        const index = metadata_ids.findIndex(object => {
-            return object === value;
-
-        });
-        
-        console.log(index);
-
-
-        let bar_sample= (sample_vals[index].slice(0,10))
-        let bar_labels= otu_idss[index].slice(0,10).reverse() 
-        let bar_text= otu_labelss[index].slice(0,10).reverse()
-        bar_sample.sort(function compareFunction(firstNum, secondNum) {
-            // resulting order is (1, 2, 3)
-            return firstNum - secondNum;
-        });
-        
-       console.log(bar_sample);
-
-        let metadataa =data.metadata[index]
-
-        
-        d3.select('#sample-metadata').append("h6").text('id: '+metadataa['id']);
-        d3.select('#sample-metadata').append("h6").text('ethnicity: '+metadataa['ethnicity']);
-        d3.select('#sample-metadata').append("h6").text('gender: '+metadataa['gender']);
-        d3.select('#sample-metadata').append("h6").text('age: '+metadataa['age']);
-        d3.select('#sample-metadata').append("h6").text('location: '+metadataa['location']);
-        d3.select('#sample-metadata').append("h6").text('bbtype: '+metadataa['bbtype']);
-        d3.select('#sample-metadata').append("h6").text('wfreq: '+metadataa['wfreq']);
-
-        // console.log(sample_metadata)
-        
-        let trace1 = {
-          y: bar_labels.map(otuID => `OTU ${otuID}`),
-          x: bar_sample,
-          text: bar_text,
-          type: 'bar',
-          orientation: 'h'
-        }
-        let data1= [trace1]
-        Plotly.newPlot('bar', data1);
-
-
-        let bubble_otu= (otu_idss[index]);
-        let bubble_sample= (sample_vals[index]);
-
-        console.log(bubble_sample);
-
-        let trace2 = {
-            x: (bubble_otu),
-            y: (bubble_sample),
-            mode: 'markers',
-            marker: {
-              color: (bubble_otu),
-              size: (bubble_sample)
-            }
-        };
-        let data2=[trace2]
-        Plotly.newPlot('bubble', data2);
-          
     });
+
 }
+
+//function which is called when a new number is selected from the dropdown
 function optionChanged(value) {
     console.log(value);
 }
+
+//function to make visuals
+function buildCharts(data,value){
+
+    sample_vals=[];
+    otu_idss=[];
+    otu_labelss=[];
+    metadata_ids=[];
+
+    for (let i = 0; i<data.samples.length; i++){
+        sample_vals.push( data.samples[i]['sample_values'])
+        otu_idss.push(data.samples[i]['otu_ids'])
+        otu_labelss.push(data.samples[i]['otu_labels'])
+        metadata_ids.push(data.metadata[i]['id'])
+    }    
+    console.log(metadata_ids);
+
+    
+
+    const index = metadata_ids.findIndex(object => {
+        return object === value;
+    });
+    
+    console.log(index);
+    
+    
+
+
+    //demographic info
+    let metadataa =data.metadata[1]
+
+    d3.select('#sample-metadata').append("h6").text('id: '+metadataa['id']);
+    d3.select('#sample-metadata').append("h6").text('ethnicity: '+metadataa['ethnicity']);
+    d3.select('#sample-metadata').append("h6").text('gender: '+metadataa['gender']);
+    d3.select('#sample-metadata').append("h6").text('age: '+metadataa['age']);
+    d3.select('#sample-metadata').append("h6").text('location: '+metadataa['location']);
+    d3.select('#sample-metadata').append("h6").text('bbtype: '+metadataa['bbtype']);
+    d3.select('#sample-metadata').append("h6").text('wfreq: '+metadataa['wfreq']);
+
+    
+    //bar graph
+
+    let bar_sample= (sample_vals[1].slice(0,10))
+    let bar_labels= otu_idss[1].slice(0,10).reverse() 
+    let bar_text= otu_labelss[1].slice(0,10).reverse()
+    bar_sample.sort(function compareFunction(firstNum, secondNum) {
+        // resulting order is (1, 2, 3)
+        return firstNum - secondNum;
+    });
+
+    console.log(bar_sample);
+
+    let trace1 = {
+    y: bar_labels.map(otuID => `OTU ${otuID}`),
+    x: bar_sample,
+    text: bar_text,
+    type: 'bar',
+    orientation: 'h'
+    }
+    let data1= [trace1]
+    Plotly.newPlot('bar', data1);
+
+    //bubble graph
+    let bubble_otu= (otu_idss[1]);
+    let bubble_sample= (sample_vals[1]);
+
+    console.log(bubble_sample);
+
+    let trace2 = {
+        x: (bubble_otu),
+        y: (bubble_sample),
+        mode: 'markers',
+        marker: {
+        color: (bubble_otu),
+        size: (bubble_sample)
+        }
+    };
+    let data2=[trace2]
+    Plotly.newPlot('bubble', data2);
+
+}
+          
+
+
 
 init();
 
