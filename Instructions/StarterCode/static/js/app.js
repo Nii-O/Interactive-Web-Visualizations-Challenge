f452874c0c65286cf4f58ba3d570503e4a59978f
Nii <inodarteifio@gmail.com>
2022-04-14 20:19:52 -0400

    upload

--- Instructions/StarterCode/static/js/app.js
+++ Instructions/StarterCode/static/js/app.js
@@ -3,126 +3,268 @@ function init() {
         console.log("read samples");
         // console.log(data);
         // console.log(data.samples[1]['sample_values'])
-        buildCharts(data)
+        //optionChanged()
+       
         var dropdown = d3.select('#selDataset');
             data.names.forEach((name)=>{
-            dropdown.append('option').text(name).property('value',name);
+            dropdown.append('option').text(name);
         })
+
+   
+
+    //function to make visuals
+    function buildCharts(){
+        defaultDataset = data.samples.filter(sample => sample.id === "940")[0];
+        //console.log(defaultDataset);
+        //optionChanged(value)
+    
+        sample_vals=defaultDataset.sample_values;
+        otu_idss=defaultDataset.otu_ids;
+        otu_labelss=defaultDataset.otu_labels;
+            
+        //console.log(otu_labelss);
+
+
+        //demographic info
+    
+
+        demoInfo = data.metadata.filter(sample => sample.id === 940)[0];
+        console.log(demoInfo);
+
+        d3.select('#sample-metadata').append("h6").text('id: '+demoInfo['id']);
+        d3.select('#sample-metadata').append("h6").text('ethnicity: '+demoInfo['ethnicity']);
+        d3.select('#sample-metadata').append("h6").text('gender: '+demoInfo['gender']);
+        d3.select('#sample-metadata').append("h6").text('age: '+demoInfo['age']);
+        d3.select('#sample-metadata').append("h6").text('location: '+demoInfo['location']);
+        d3.select('#sample-metadata').append("h6").text('bbtype: '+demoInfo['bbtype']);
+        d3.select('#sample-metadata').append("h6").text('wfreq: '+demoInfo['wfreq']);
+
+        
+        //bar graph
+
+        let bar_sample= (sample_vals.slice(0,10))
+        let bar_labels= otu_idss.slice(0,10).reverse() 
+        let bar_text= otu_labelss.slice(0,10).reverse()
+        bar_sample.sort(function compareFunction(firstNum, secondNum) {
+            // resulting order is (1, 2, 3)
+            return firstNum - secondNum;
+        });
+
+        //console.log(bar_text);
+
+        let trace1 = {
+            y: bar_labels.map(otuID => `OTU ${otuID}`),
+            x: bar_sample,
+            text: bar_text,
+            type: 'bar',
+            orientation: 'h'
+        }
+        let data1= [trace1]
+        Plotly.newPlot('bar', data1);
+
+        //bubble graph
+        let bubble_otu= (otu_idss);
+        let bubble_sample= (sample_vals);
+
+        //console.log(bubble_sample);
+
+        let trace2 = {
+            x: (bubble_otu),
+            y: (bubble_sample),
+            text: (otu_labelss),
+            mode: 'markers',
+            marker: {
+            color: (bubble_otu),
+            size: (bubble_sample),
+            colorscale: 'Earth'
+            }
+        };
+        
+        let bubbleLayout= {
+            title: 'Bacteria Cultures per Sample',
+            margin: {t:0},
+            hovermode: "closest",
+            xaxis: {title: 'OTU ID'},
+            margin: {t:30}
+        };
+
+        let data2=[trace2]
+        Plotly.newPlot('bubble', data2, bubbleLayout);
+
+
+
+        let wfreqInfo = demoInfo.wfreq;
+        //console.log(wfreqInfo);
+
+        let gaugeData = [
+            {
+                domain: { x: [0, 1], y: [0, 1] },
+                value: wfreqInfo,
+                title: {text: '<b>Belly Button Washing Frequency</b> <br> Scrubs per week'},
+                type: "indicator",
+                mode: "gauge+number",
+                gauge: {
+                    axis: { range: [null, 9] },
+                    steps: [
+                        { range: [0, 1], color: 'rgb(248, 243, 236)' },
+                        { range: [1, 2], color: 'rgb(244, 241, 229)' },
+                        { range: [2, 3], color: 'rgb(233, 230, 202)' },
+                        { range: [3, 4], color: 'rgb(229, 231, 179)' },
+                        { range: [4, 5], color: 'rgb(213, 228, 157)' },
+                        { range: [5, 6], color: 'rgb(183, 204, 146)' },
+                        { range: [6, 7], color: 'rgb(140, 191, 136)' },
+                        { range: [7, 8], color: 'rgb(138, 187, 143)' },
+                        { range: [8, 9], color: 'rgb(133, 180, 138)' },
+                    ],
+                }
+            }
+        ];
+            
+        let gaugeLayout = { 
+            width: 600, 
+            height: 450, 
+            margin: { t: 0, b: 0 } 
+        };
+        
+        Plotly.newPlot('gauge', gaugeData, gaugeLayout);
+
+    }
+
+
+    buildCharts();
+
+
+    d3.select('#selDataset').on('change',optionChanged);
+
+
+    function optionChanged() {
+            
+        let dropdown1=d3.select('#selDataset');
+
+        let dropval= dropdown1.property('value');
+        console.log(dropval);
+
+
+        dataset = data.samples.filter(sample => sample.id === dropval)[0];
+        console.log(dataset);
+
+        sample_vals=dataset.sample_values;
+        otu_idss=dataset.otu_ids;
+        otu_labelss=dataset.otu_labels;
+            
+        //console.log(otu_labelss);
         
 
+        //demographic info
 
+        demoInfo1 = data.metadata.filter(sample => sample.id === dropval)[0];
+        
+        console.log(demoInfo1);
 
-    });
+        d3.select('#sample-metadata').append("h6").text('id: '+demoInfo1['id']);
+        d3.select('#sample-metadata').append("h6").text('ethnicity: '+demoInfo1['ethnicity']);
+        d3.select('#sample-metadata').append("h6").text('gender: '+demoInfo1['gender']);
+        d3.select('#sample-metadata').append("h6").text('age: '+demoInfo1['age']);
+        d3.select('#sample-metadata').append("h6").text('location: '+demoInfo1['location']);
+        d3.select('#sample-metadata').append("h6").text('bbtype: '+demoInfo1['bbtype']);
+        d3.select('#sample-metadata').append("h6").text('wfreq: '+demoInfo1['wfreq']);
 
-}
+        
+        //bar graph
 
-//function which is called when a new number is selected from the dropdown
-function optionChanged(value) {
-    console.log(value);
-}
+        let bar_sample= (sample_vals.slice(0,10))
+        let bar_labels= otu_idss.slice(0,10).reverse() 
+        let bar_text= otu_labelss.slice(0,10).reverse()
+        bar_sample.sort(function compareFunction(firstNum, secondNum) {
+            // resulting order is (1, 2, 3)
+            return firstNum - secondNum;
+        });
 
-//function to make visuals
-function buildCharts(data,value){
+        console.log(bar_text);
 
-    sample_vals=[];
-    otu_idss=[];
-    otu_labelss=[];
-    metadata_ids=[];
-
-    for (let i = 0; i<data.samples.length; i++){
-        sample_vals.push( data.samples[i]['sample_values'])
-        otu_idss.push(data.samples[i]['otu_ids'])
-        otu_labelss.push(data.samples[i]['otu_labels'])
-        metadata_ids.push(data.metadata[i]['id'])
-    }    
-    console.log(metadata_ids);
-
-    
-
-    const index = metadata_ids.findIndex(object => {
-        return object === value;
-    });
-    
-    console.log(index);
-    
-    
-
-
-    //demographic info
-    let metadataa =data.metadata[1]
-
-    d3.select('#sample-metadata').append("h6").text('id: '+metadataa['id']);
-    d3.select('#sample-metadata').append("h6").text('ethnicity: '+metadataa['ethnicity']);
-    d3.select('#sample-metadata').append("h6").text('gender: '+metadataa['gender']);
-    d3.select('#sample-metadata').append("h6").text('age: '+metadataa['age']);
-    d3.select('#sample-metadata').append("h6").text('location: '+metadataa['location']);
-    d3.select('#sample-metadata').append("h6").text('bbtype: '+metadataa['bbtype']);
-    d3.select('#sample-metadata').append("h6").text('wfreq: '+metadataa['wfreq']);
-
-    
-    //bar graph
-
-    let bar_sample= (sample_vals[1].slice(0,10))
-    let bar_labels= otu_idss[1].slice(0,10).reverse() 
-    let bar_text= otu_labelss[1].slice(0,10).reverse()
-    bar_sample.sort(function compareFunction(firstNum, secondNum) {
-        // resulting order is (1, 2, 3)
-        return firstNum - secondNum;
-    });
-
-    console.log(bar_sample);
-
-    let trace1 = {
-    y: bar_labels.map(otuID => `OTU ${otuID}`),
-    x: bar_sample,
-    text: bar_text,
-    type: 'bar',
-    orientation: 'h'
-    }
-    let data1= [trace1]
-    Plotly.newPlot('bar', data1);
-
-    //bubble graph
-    let bubble_otu= (otu_idss[1]);
-    let bubble_sample= (sample_vals[1]);
-
-    console.log(bubble_sample);
-
-    let trace2 = {
-        x: (bubble_otu),
-        y: (bubble_sample),
-        mode: 'markers',
-        marker: {
-        color: (bubble_otu),
-        size: (bubble_sample)
+        let trace1 = {
+            y: bar_labels.map(otuID => `OTU ${otuID}`),
+            x: bar_sample,
+            text: bar_text,
+            type: 'bar',
+            orientation: 'h'
         }
-    };
-    let data2=[trace2]
-    Plotly.newPlot('bubble', data2);
+        let data1= [trace1]
+        Plotly.newPlot('bar', data1);
+
+        //bubble graph
+        let bubble_otu= (otu_idss);
+        let bubble_sample= (sample_vals);
+
+        console.log(bubble_sample);
+
+        let trace2 = {
+            x: (bubble_otu),
+            y: (bubble_sample),
+            text: (otu_labelss),
+            mode: 'markers',
+            marker: {
+            color: (bubble_otu),
+            size: (bubble_sample),
+            colorscale: 'Earth'
+            }
+        };
+        
+        let bubbleLayout= {
+            title: 'Bacteria Cultures per Sample',
+            margin: {t:0},
+            hovermode: "closest",
+            xaxis: {title: 'OTU ID'},
+            margin: {t:30}
+        };
+
+        let data2=[trace2]
+        Plotly.newPlot('bubble', data2, bubbleLayout);
+
+
+
+        let wfreqInfo = demoInfo.wfreq;
+
+        let gaugeData = [
+            {
+                domain: { x: [0, 1], y: [0, 1] },
+                value: wfreqInfo,
+                title: {text: '<b>Belly Button Washing Frequency</b> <br> Scrubs per week'},
+                type: "indicator",
+                mode: "gauge+number",
+                gauge: {
+                    axis: { range: [null, 9] },
+                    steps: [
+                        { range: [0, 1], color: 'rgb(248, 243, 236)' },
+                        { range: [1, 2], color: 'rgb(244, 241, 229)' },
+                        { range: [2, 3], color: 'rgb(233, 230, 202)' },
+                        { range: [3, 4], color: 'rgb(229, 231, 179)' },
+                        { range: [4, 5], color: 'rgb(213, 228, 157)' },
+                        { range: [5, 6], color: 'rgb(183, 204, 146)' },
+                        { range: [6, 7], color: 'rgb(140, 191, 136)' },
+                        { range: [7, 8], color: 'rgb(138, 187, 143)' },
+                        { range: [8, 9], color: 'rgb(133, 180, 138)' },
+                    ],
+                }
+            }
+        ];
+            
+        let gaugeLayout = { 
+            width: 600, 
+            height: 450, 
+            margin: { t: 0, b: 0 } 
+        };
+        
+        Plotly.newPlot('gauge', gaugeData, gaugeLayout);
+
+
+        }
+
+
+    });
 
 }
-          
-
-
 
 init();
 
-// var barData = [
-//     {
-//       y: yticks,
-//       x: sample_values.slice(0, 10).reverse(),
-//       text: otu_labels.slice(0, 10).reverse(),
-//       type: "bar",
-//       orientation: "h",
-//     }
-//   ];
-//   // Create the layout for the bar chart. 
-//   var barLayout = {
-//     title: "Top 10 Bacteria Cultures Found",
-//     margin: { t: 30, l: 150 }
-//   };
-//   // Use Plotly to plot the data with the layout. 
-//   Plotly.newPlot("bar", barData, barLayout);
 
-
-//   use function buildCharts(sample) from line 7 to 54
